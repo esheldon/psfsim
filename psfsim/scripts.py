900f28a6e4675ae7c05579b7fbaf0444c8e994e9
Erin Sheldon <erin.sheldon@gmail.com>
2016-07-18 12:49:15 -0400

    docs

--- psfsim/scripts.py
+++ psfsim/scripts.py
@@ -3,9 +3,24 @@ from __future__ import print_function
 from . import files
 
 class ScriptWriter(dict):
-    def __init__(self, run, extra_commands=''):
+    """
+    class to write scripts and queue submission scripts
+
+    parameters
+    ----------
+    run: string
+        run identifier
+    system: string
+        Queue system.  Currently supports wq.
+    extra_commands: string
+        Extra shell commands to run, e.g. for setting up
+        your environment
+    """
+
+    def __init__(self, run, extra_commands='', system='wq'):
         self['run'] = run
         self['extra_commands'] = extra_commands
+        self['system'] = system
 
         self._load_config()
         self._makedirs()
@@ -13,13 +28,21 @@ class ScriptWriter(dict):
         self['njobs']=self.conf['output']['nfiles']
 
     def write_scripts(self):
-
+        """
+        write the basic bash scripts and queue submission scripts
+        """
         for i in xrange(self['njobs']):
+            if self['system']=='wq':
+                self._write_wq(i)
+            else:
+                raise RuntimeError("bad system: '%s'" % self['system'])
+
             self._write_script(i)
-            self._write_wq(i)
 
     def _write_script(self, index):
-
+        """
+        write the basic bash script
+        """
         self['jobnum'] = index + 1
         # temporary
         self['config_file'] = self['stars_config_file'],
@@ -32,6 +55,9 @@ class ScriptWriter(dict):
             fobj.write(text)
 
     def _write_wq(self, index):
+        """
+        write the wq submission script
+        """
         wq_fname=files.get_wq_file(self['run'], index)
 
         self['job_name'] = 'psfsim-stars-%s-%04d.yaml' % (self['run'], index)
@@ -44,6 +70,9 @@ class ScriptWriter(dict):
             fobj.write(text)
 
     def _makedirs(self):
+        """
+        make all the directories needed
+        """
         script_dir = files.get_script_dir(self['run'])
 
         dirs=[script_dir]
@@ -57,6 +86,9 @@ class ScriptWriter(dict):
                     pass
 
     def _load_config(self):
+        """
+        load the galsim config and do some checks
+        """
         self['stars_config_file']=files.get_stars_config_file(self['run'])
 
         self.conf = files.read_stars_config(self['run'])
