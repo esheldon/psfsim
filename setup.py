1b98df4975a903a4f9feb809f66e56844bfbb4d5
Erin Sheldon <erin.sheldon@gmail.com>
2016-07-18 13:12:39 -0400

    install psfsim-make-scripts command

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
+import os
 import distutils
 from distutils.core import setup, Extension, Command
 import numpy
 
+scripts=['psfsim-make-scripts']
+
+scripts=[os.path.join('bin',s) for s in scripts]
+
 setup(
     name="psfsim", 
     packages=['psfsim'],
+    scripts=scripts,
     version="0.1",
 )
 
